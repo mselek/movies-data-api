8cc4f9c525ad28d05a2e4971b197e892bb4f0dab
Mélik <m.selek@hotmail.fr>
2023-02-13 14:57:45 +0100

    edit links

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 /* Modules */
 import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 
 /* Pages */
 import Home from './pages/Home';
@@ -8,12 +8,18 @@ import Error from './pages/Error';
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="https://mselek.github.io/movies-data-api" exact element={<Home />} />
-        <Route path="*" exact element={<Error />} />
-      </Routes>
-    </BrowserRouter>
+    <HashRouter>
+        <Routes>
+          <Route path="/" exact element={<Home />} />
+          <Route path="*" exact element={<Error />} />
+        </Routes>
+    </HashRouter>
+    // <BrowserRouter>
+    //   <Routes>
+    //     <Route path="https://mselek.github.io/movies-data-api" exact element={<Home />} />
+    //     <Route path="*" exact element={<Error />} />
+    //   </Routes>
+    // </BrowserRouter>
   );
 };
 
